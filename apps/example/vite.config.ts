77d90aa20a2b90b8760930e03579fb0106202bf0
Cory Deppen <cory@deppen.us>
2026-04-07 18:13:30 -0400

    use Vite+ (#4)

--- apps/example/vite.config.ts
+++ apps/example/vite.config.ts
@@ -1,6 +1,6 @@
 import stylex from "@stylexjs/unplugin";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 
 // https://vite.dev/config/
 export default defineConfig({
--- packages/cli/tsdown.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "tsdown";
-
-export default defineConfig({
-  entry: ["./src/{index,run}.ts"],
-  exports: true,
-});
